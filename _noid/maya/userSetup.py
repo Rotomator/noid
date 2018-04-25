454e39395820d6fb271b973229b66f6c1f278c93
aganzerli <aganzerli@hotmail.com>
2018-04-25 16:24:21 +0200

    commit aganzerli

--- _noid/maya/userSetup.py
+++ _noid/maya/userSetup.py
@@ -32,37 +32,38 @@ def noid_setEnvs(envs) :
         noid_setEnv(envs[i], envs[i+1])
 
 
+''' envs
+    ---------------------------------------------------------------------------------------------------------------------------- '''
+MAYA_VERSION= os.getenv("MAYA_VERSION")
+VRAY_VERSION= os.getenv("VRAY_VERSION")
+ARCHIVE_VERSION= os.getenv("ARCHIVE_VERSION")
+
+noid_setEnvs([
+	'ARCHIVE_PATH'					                , '%NOID_PATH%/archive/'+ARCHIVE_VERSION,
+
+	'+VRAY_FOR_MAYA'+MAYA_VERSION+'_MAIN_x64'	    , '%NOID_BIN_PATH%/vray/'+VRAY_VERSION+'/maya'+MAYA_VERSION+'/maya_vray',
+	'+VRAY_FOR_MAYA'+MAYA_VERSION+'_PLUGINS_x64'    , '%VRAY_FOR_MAYA'+MAYA_VERSION+'_MAIN_x64%/vrayplugins',
+	'+VRAY_OSL_PATH_MAYA'+MAYA_VERSION+'_x64'	    , '%VRAY_FOR_MAYA'+MAYA_VERSION+'_PLUGINS_x64%',
+	'+VRAY_AUTH_CLIENT_FILE_PATH'	                , '%NOID_PATH%/maya/vray',
+    '+PATH'                                         , '%NOID_BIN_PATH%/vray/'+VRAY_VERSION+'/maya'+MAYA_VERSION+'/maya_root/bin',
+	'+MAYA_PLUG_IN_PATH'			                , '%VRAY_FOR_MAYA'+MAYA_VERSION+'_MAIN_x64%/plug-ins',
+	'+MAYA_SCRIPT_PATH'				                , '%VRAY_FOR_MAYA'+MAYA_VERSION+'_MAIN_x64%/scripts',
+	'+XBMLANGPATH'					                , '%VRAY_FOR_MAYA'+MAYA_VERSION+'_MAIN_x64%/icons',
+
+	'+VRAY_FOR_MAYA'+MAYA_VERSION+'_PLUGINS_x64'	, '%ARCHIVE_PATH%/vray/'+VRAY_VERSION,
+	'+MAYA_PLUG_IN_PATH'			                , '%ARCHIVE_PATH%/maya/plug-ins/'+MAYA_VERSION,
+	'+MAYA_SCRIPT_PATH'				                , '%ARCHIVE_PATH%/maya/scripts',
+	'+XBMLANGPATH'				                    , '%ARCHIVE_PATH%/maya/icons/',
+
+    'RR_ROOT'                                       , '//storb/diskb/RoyalRender',
+	'+MAYA_PLUG_IN_PATH'			                , '%NOID_PATH%/maya/royalrender'
+])
+
+
 ''' python modules path
     ---------------------------------------------------------------------------------------------------------------------------- '''
 sys.path.append(noid_path('%NOID_PATH%/pythonModules'))
-
-
-''' envs
-    ---------------------------------------------------------------------------------------------------------------------------- '''
-noid_setEnvs([
-	'MAYA_VERSION'					    , '2016',
-	'VRAY_VERSION'					    , '35201',
-	'ARCHIVE_VERSION'				    , '0000_0013',
-
-	'ARCHIVE_PATH'					    , '%NOID_PATH%/archive/%ARCHIVE_VERSION%',
-
-	'+VRAY_FOR_MAYA2016_MAIN_x64'	    , '%NOID_BIN_PATH%/vray/%VRAY_VERSION%/maya%MAYA_VERSION%/maya_vray',
-	'+VRAY_FOR_MAYA2016_PLUGINS_x64'	, '%VRAY_FOR_MAYA2016_MAIN_x64%/vrayplugins',
-	'+VRAY_OSL_PATH_MAYA2016_x64'	    , '%VRAY_FOR_MAYA2016_PLUGINS_x64%',
-	'+VRAY_AUTH_CLIENT_FILE_PATH'	    , '%NOID_PATH%/maya/vray',
-    '+PATH'                             , '%NOID_BIN_PATH%/vray/%VRAY_VERSION%/maya%MAYA_VERSION%/maya_root/bin',
-	'+MAYA_PLUG_IN_PATH'			    , '%VRAY_FOR_MAYA2016_MAIN_x64%/plug-ins',
-	'+MAYA_SCRIPT_PATH'				    , '%VRAY_FOR_MAYA2016_MAIN_x64%/scripts',
-	'+XBMLANGPATH'					    , '%VRAY_FOR_MAYA2016_MAIN_x64%/icons',
-
-	'+VRAY_FOR_MAYA2016_PLUGINS_x64'	, '%ARCHIVE_PATH%/vray/%VRAY_VERSION%',
-	'+MAYA_PLUG_IN_PATH'			    , '%ARCHIVE_PATH%/maya/plug-ins/%MAYA_VERSION%',
-	'+MAYA_SCRIPT_PATH'				    , '%ARCHIVE_PATH%/maya/scripts',
-	'+XBMLANGPATH'				        , '%ARCHIVE_PATH%/maya/icons/',
-
-    'RR_ROOT'                           , '//storb/diskb/RoyalRender',
-	'+MAYA_PLUG_IN_PATH'			    , '%NOID_PATH%/maya/royalrender'
-])
+sys.path.append(noid_path('%NOID_BIN_PATH%/vray/'+VRAY_VERSION+'/maya'+MAYA_VERSION+'/maya_vray/scripts'))
 
 
 ''' royalRender
