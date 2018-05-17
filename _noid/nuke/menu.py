f79ef7c6bcdecfc1f86c31a1149c75a263d45a0e
cshafaghi <mmchervin@gmail.com>
2018-05-17 16:48:13 +0200

    commit cshafaghi

--- _noid/nuke/menu.py
+++ _noid/nuke/menu.py
@@ -10,3 +10,6 @@ noidMenu= nukeMenu.addMenu("NOID")
 noidMenu.addCommand("Set Current Task", "")
 noidMenu.addSeparator()
 noidMenu.addCommand("Increment", "")
+
+
+import LensDistort.LensDistort_3de
