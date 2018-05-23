586626e6c804affd602962c099a039ddaddd7c1e
cshafaghi <mmchervin@gmail.com>
2018-05-23 15:59:34 +0200

    commit cshafaghi

--- _noid/toolbars/_nuke10.bat
+++ _noid/toolbars/_nuke10.bat
@@ -1,4 +1,4 @@
 SET PYTHONPATH=%NOID_PATH%\pythonModules
-SET NUKE_PATH=%NOID_PATH%\nuke;%NOID_BIN_PATH%\nuke;%NOID_PATH%\nuke\gizmos
+SET NUKE_PATH=%NOID_PATH%\nuke;%NOID_BIN_PATH%\nuke;%NOID_BIN_PATH%\nuke\LensDistort\Nuke10.5;%NOID_PATH%\nuke\gizmos
 
 "C:\Program Files\Nuke10.5v5\Nuke10.5.exe"
