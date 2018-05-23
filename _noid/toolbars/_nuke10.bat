6d3af0222ef1e219e629baade080b36507323b90
aganzerli <aganzerli@hotmail.com>
2018-05-23 16:37:11 +0200

    commit

--- _noid/toolbars/_nuke10.bat
+++ _noid/toolbars/_nuke10.bat
@@ -1,3 +1,4 @@
-SET PYTHONPATH=C:\_noid\pythonModules
+SET PYTHONPATH=%NOID_PATH%\pythonModules
+SET NUKE_PATH=%NOID_PATH%\nuke;%NOID_BIN_PATH%\nuke;%NOID_PATH%\nuke\gizmos
 
-"C:\Program Files\Nuke10.5v7\Nuke10.5.exe"
+"C:\Program Files\Nuke10.5v5\Nuke10.5.exe"
