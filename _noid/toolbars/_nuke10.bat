a7749880ab58e6823375a3cc85d57bf6eccc3941
aganzerli <aganzerli@hotmail.com>
2018-05-23 17:24:56 +0200

    commit

--- _noid/toolbars/_nuke10.bat
+++ _noid/toolbars/_nuke10.bat
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 SET PYTHONPATH=%NOID_PATH%\pythonModules
 SET NUKE_PATH=%NOID_PATH%\nuke;%NOID_BIN_PATH%\nuke;%NOID_BIN_PATH%\nuke\LensDistort\Nuke10.5;%NOID_PATH%\nuke\gizmos
-=======
-<<<<<<< HEAD
-SET PYTHONPATH=%NOID_PATH%\pythonModules
-SET NUKE_PATH=%NOID_PATH%\nuke;%NOID_BIN_PATH%\nuke;%NOID_BIN_PATH%\nuke\LensDistort\Nuke10.5;%NOID_PATH%\nuke\gizmos
-=======
-SET PYTHONPATH=C:\_noid\pythonModules
->>>>>>> 72545dcbb86cac49b4491d4891792bfc1906fb97
->>>>>>> beb0fb2674bc88de2addfea596a0ee2581f59d42
 
 "C:\Program Files\Nuke10.5v5\Nuke10.5.exe"
